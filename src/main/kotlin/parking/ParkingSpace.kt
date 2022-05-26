47426be3d67ad46b4b00dc4a540089942b9045eb
Luciano Menendez <lumenendez@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-05-26 17:30:24 -0300

    push of branch exercice-7

--- src/main/kotlin/parking/ParkingSpace.kt
+++ src/main/kotlin/parking/ParkingSpace.kt
@@ -1,13 +1,26 @@
 package parking
 
 import vehicle.Vehicle
+import vehicle.VehicleType
 import java.util.*
 
-data class ParkingSpace(var vehicle: Vehicle) {
+data class ParkingSpace(var vehicle: Vehicle, val parking: Parking) {
     companion object {
-        const val MINUTES_IN_MILISECONDS = 60_000
+        const val MINUTES_IN_MILLISECONDS = 60_000
+    }
+
+    /*
+    * Exercise 6
+    *
+    * */
+    fun checkOutVehicle(plate: String, onSuccess: (Int) -> Unit, onError: () -> Unit) {
+        val vehicle = parking.vehicles.firstOrNull { it.plate == plate }
+        vehicle?.let {
+            parking.vehicles.remove(it)
+            onSuccess(1)
+        }?: onError()
     }
 
     val parkedTime: Long
-        get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis) / MINUTES_IN_MILISECONDS
+        get() = (Calendar.getInstance().timeInMillis - vehicle.checkInTime.timeInMillis) / MINUTES_IN_MILLISECONDS
 }
